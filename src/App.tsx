43cdf4596d642013a943bf92c6ea03bc05c626ea
Nicholas Farm <nicholasfarm27@gmail.com>
2021-08-20 20:50:04 +0800

    Add the 'persist dark mode' function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
 import "./styles/theme.scss";
 import styles from "./App.module.scss";
-import { useAppSelector } from "./store/hooks";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { settingsActions } from "./store/settings-slice";
 
 function App() {
     const darkTheme = useAppSelector((state) => state.settings.darkTheme);
-    // const dispatch = useAppDispatch();
-
-    // const changeThemeHandler = (e: React.MouseEvent) => {
-    //     dispatch(settingsActions.changeTheme());
-    // };
-
     const theme = darkTheme ? "dark" : "light";
+    const storedTheme = localStorage.getItem("THEME");
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (storedTheme !== theme) {
+            dispatch(settingsActions.changeTheme())
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("THEME", theme)
+    }, [theme])
 
     return (
         <div className={`${styles.App} ${theme}`}>
             <Header />
             <Body />
-            {/* <button onClick={changeThemeHandler}>Change Theme</button> */}
         </div>
     );
 }
